414a0e08a8d13d705bcef793f3602bd1a546f83a
Dima Moruha <trluxsus@gmail.com>
2019-11-19 19:23:57 +0200

    Sys Lab-5 Add class Student

---
+++ StudentsAndMarks/Student.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace StudentsAndMarks
+{
+    class Student
+    {
+        private string _Name;
+
+        public string Name
+        {
+            get { return _Name; }
+            set
+            {
+                if (String.IsNullOrWhiteSpace(value))
+                {
+                    MessageBox.Show("Name can't be empty or contain white spaces", "Warning");
+                    return;
+                }
+
+                _Name = value;
+            }
+        }
+    }
+}
